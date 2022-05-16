c61b4d0f60f7ac01b6cf6b80c1dc03ae71d3792b
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-05-16 16:41:01 +0200

    start

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Un IMDBad bien moche
+
+Backend PHP Docker, donc pour faire tourner tout ça :
+````shell
+docker-compose up -d &&
+cd ./frontend &&
+npm start
+````
+
+Puis penser à effectuer :
+````shell
+npm install #dans /frontend/
+
+composer install #dans /backend/
+````
+
+Vous pouvez créer vos propres identifiants, mais de base :
+- FrancisHuster : password
+- JohnBob : password
---
+++ database/data.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db
+-- Généré le : lun. 16 mai 2022 à 14:21
+-- Version du serveur : 10.7.3-MariaDB-1:10.7.3+maria~focal
+-- Version de PHP : 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Movie`
+--
+
+CREATE TABLE `Movie` (
+  `id` int(11) NOT NULL,
+  `authorId` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `datesortie` datetime NOT NULL,
+  `review` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `User`
+--
+
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Movie`
+--
+ALTER TABLE `Movie`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `authodIr` (`authorId`);
+
+--
+-- Index pour la table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Movie`
+--
+ALTER TABLE `Movie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
